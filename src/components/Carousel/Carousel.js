319761fe93e6676888f490954ca4da0e082c6d63
Tejeshwar Dutta <tejdutta7051@gmail.com>
2023-01-16 22:32:29 +0530

    first-commit

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Carousel.css";
+
+function Carousel() {
+
+  return (
+    <div className="carousel">
+
+      <img className="carousel_images" src="https://m.media-amazon.com/images/I/81EZPF-FSdL._SX3000_.jpg" />
+    </div>
+  );
+}
+export default Carousel;
